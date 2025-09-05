a6a1c53a4ac818410df975ce060bdc0f91289b78
Gonzalonoe <gonza2002noe@gmail.com>
2025-09-05 16:03:42 -0300

    1.0.3

---
+++ app/src/main/java/com/example/viewbindingtp2/LibroActivityViewModel.java
@@ -0,0 +1,10 @@
+package com.example.viewbindingtp2;
+
+import androidx.lifecycle.AndroidViewModel;
+
+public class LibroActivityViewModel extends AndroidViewModel {
+
+    public LibroActivityViewModel(android.app.Application application){
+        super(application);
+    }
+}
---
+++ app/src/main/java/com/example/viewbindingtp2/MainActivityViewModel.java
@@ -0,0 +1,11 @@
+package com.example.viewbindingtp2;
+
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    public MainActivityViewModel(android.app.Application application){
+        super(application);
+    }
+
+}
